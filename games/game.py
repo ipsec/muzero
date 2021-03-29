New tests and fixes in mcts and backpropagation

games/game.py
@@ -105,9 +105,7 @@ class Game(object):
         observation, reward, done, info = self.env.step(action.index)
         self.done = done
 
-        if not done:
-            self.states.append(np.atleast_2d(observation))
-
+        self.states.append(np.atleast_2d(observation))
         self.rewards.append(reward)
         self.history.append(action)
 
@@ -194,9 +192,9 @@ def make_atari_config(env: Env) -> MuZeroConfig:
         max_moves=700,  # Half an hour at action repeat 4.
         discount=0.997,
         dirichlet_alpha=0.25,
-        num_simulations=50,  # Number of future moves self-simulated
+        num_simulations=25,  # Number of future moves self-simulated
         batch_size=128,
-        td_steps=10,  # Number of steps in the future to take into account for calculating the target value
+        td_steps=500,  # Number of steps in the future to take into account for calculating the target value
         num_actors=5,
         training_steps=100000,
         checkpoint_interval=10,

muzero.py
@@ -58,7 +58,7 @@ def play_game(config: MuZeroConfig, network: Network) -> Game:
         current_observation = game.make_image(-1)
         network_output = network.initial_inference(current_observation)
         expand_node(root, game.to_play(), game.legal_actions(), network_output)
-        backpropagate([root], network_output.value, game.to_play(), config.discount, min_max_stats)
+        # backpropagate([root], network_output.value, game.to_play(), config.discount, min_max_stats)
         add_exploration_noise(config, root)
 
         # We then run a Monte Carlo Tree Search using only action sequences and the
@@ -168,12 +168,6 @@ def compute_loss(network: Network, batch, weight_decay: float):
             gradient_scale, network_output = prediction
             target_value, target_reward, target_policy = target
 
-            if target_reward != 1.0 and target_reward != 0.0:
-                print(f"value: {target_value}")
-                print(f"reward: {target_reward}")
-                print(f"policy: {target_policy}")
-                print("##########################")
-
             policy_loss = 0.
 
             if target_policy:
@@ -203,10 +197,10 @@ def update_weights(optimizer: tf.optimizers.Optimizer, network: Network, batch, 
     with tf.GradientTape() as tape:
         loss = compute_loss(network, batch, weight_decay)
 
-    train_loss(loss)
-
     grads = tape.gradient(loss, network.get_variables())
     optimizer.apply_gradients(zip(grads, network.get_variables()))
+
+    train_loss(loss)
 
     network.increment_training_steps()
     return loss